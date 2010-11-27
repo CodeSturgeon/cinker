69a518d963d44f067bf3d2a5f602a923082deb7f
Nick Fisher <github@nickdafish.com>
2010-11-26 21:30:45 -0500

    refactoring for Step

--- runner.js
+++ runner.js
@@ -2,25 +2,44 @@
 // Command script
 var fs = require('fs');
 var http = require('http');
+var Step = require('step');
+
 var cfg = require('./config');
 
-var ddoc_uri = '/'+cfg.db_name+'/_design/cinker/';
+// Compile cfg a litte for use elsewhere
+cfg.ddoc_uri = '/'+cfg.db_name+'/_design/cinker/';
+cfg.profiles_uri = cfg.ddoc_uri + '_view/profiles?group=true'
+cfg.couchdb = http.createClient(5984, cfg.db_host);
 
-var couchdb = http.createClient(5984, cfg.db_host);
-var req = couchdb.request('GET', ddoc_uri+'_view/profiles?group=true');
+var cget = function(uri, callback) {
+  var req = cfg.couchdb.request('GET', uri);
+  req.end();
 
-req.end();
-
-req.on('response', function(response) {
-  response.setEncoding('utf8');
-  var body = '';
-  response.on('data', function (chunk) {
-    body += chunk;
+  req.on('response', function(response) {
+    response.setEncoding('utf8');
+    var body = '';
+    response.on('data', function (chunk) { body += chunk; });
+    response.on('end', function (){
+      var result = JSON.parse(body);
+      var err = 'poop';
+      callback(err, result);
+    });
   });
-  response.on('end', function(chunk) {
-    var rows = JSON.parse(body)['rows'];
-    for (ri in rows) {
-      console.log(rows[ri]['key'][1]);
+}
+
+Step(
+  function loadcfg() {
+    cget(cfg.profiles_uri, this);
+  },
+  function launchread(err, body) {
+    var group = this.group();
+    for (ri in body['rows']) {
+      fs.readFile(body['rows'][ri]['key'][1], group());
     }
-  });
-});
+  },
+  function splat(err, buffers) {
+    for (bi in buffers) {
+      console.log(buffers[bi].toString('utf8'));
+    }
+  }
+);
