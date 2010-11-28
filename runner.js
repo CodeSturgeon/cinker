8d7d11d6eb345eed205e8eda039e4ea337ccf9c0
Nick Fisher <github@nickdafish.com>
2010-11-28 18:04:47 -0500

    rewrite for couch-client... which I now find to also be bad...

--- runner.js
+++ runner.js
@@ -2,52 +2,30 @@
 // Command script
 var fs = require('fs');
 var http = require('http');
-var crypto = require('crypto');
 
 var Step = require('step');
-var cradle = require('cradle');
+var CouchClient = require('couch-client');
 
 var cfg = require('./config');
 var watchers = require('./watchers');
 
 // Compile cfg a litte for use elsewhere
 cfg.ddoc_uri = '/'+cfg.db_name+'/_design/cinker/';
+// FIXME set profile start and end keys
 cfg.profiles_uri = cfg.ddoc_uri + '_view/profiles?group=true'
-cfg.couchdb = http.createClient(5984, cfg.db_host);
-
-var c = new(cradle.Connection)('localhost',5984,{cached:false,raw:true});
-var db = c.database('play');
+cfg.db = CouchClient('http://localhost:5984/play');
 
 var cdie = function(err, message){
   console.log(message+' '+JSON.stringify(err));
   return null;
 }
 
-// FIXME set profile start and end keys
-db.view('cinker/profiles', {group:true}, function(err, body) {
+cfg.db.request('GET', cfg.profiles_uri, function(err, body) {
   // doing this in a function for an isolated namespace
-  for (ri in body['rows']) {var x = function(){
+  for (ri in body['rows']) {
     var watch_path = body['rows'][ri]['key'][1];
     var watch_cfg = body['rows'][ri]['value']; // unused
     console.log('Setting up for: '+watch_path);
-    var md5 = crypto.createHash('md5');
-    md5.update(cfg.profile);
-    md5.update(watch_path);
-    var state_uri = 'cinker-state-'+md5.digest('hex');
-    db.get(state_uri, function(err, state){
-      if (err){
-        if(err['error']==='not_found'){
-          // No state yet! Lets make some...
-          var state = { locked: false, profile: cfg.profile, path: watch_path };
-          db.put(state_uri, state, function(err, res){
-            if(err) cdie(err, 'could not create state');
-            console.log('Making state for '+watch_path);
-            state._rev = res['rev'];
-            fs.watchFile(watch_path, watchers.fileCink(db, state, cfg));
-          }
-          );
-        } else cdie(err, 'could not get state');
-      } else fs.watchFile(watch_path, watchers.fileCink(db, state, cfg));
-    });
-  }();}
+    fs.watchFile(watch_path, watchers.fileCink(watch_path, cfg));
+  }
 });
--- watchers.js
+++ watchers.js
@@ -1,3 +1,4 @@
+var crypto = require('crypto');
 var fs = require('fs');
 var Step = require('step');
 
@@ -8,22 +9,49 @@ var cdie = function(err, message){
 
 // state: couchdb doc
 // cfg: dict of running cfg
-var fileCink = function(db, state, cfg){
+var fileCink = function(watch_path, cfg){
+  var state = {};
+  var md5 = crypto.createHash('md5');
+  md5.update(cfg.profile);
+  md5.update(watch_path);
+  var state_id = 'cinker-state-'+md5.digest('hex');
   return function(curr, prev){
     Step(
-      // Make lock
-      function make_lock(){
-        console.log('file updated: '+state['path']+' '+curr['mtime']);
-        console.log(state.locked);
-        state['locked'] = true;
-        console.log(state.locked);
-        console.log('state: '+JSON.stringify(state));
-        db.save(state['_id'], state['_rev'], state, this);
+      // Get state
+      function req_state(){
+        cfg.db.get(state_id, this);
+      },
+      // Use either the found state, or make a new state then save it
+      function lock_state(err, ret){
+        // Yeah this is broken but the couch-client lib screws up errors
+        if(err){
+          // No state yet! Lets make some...
+          state = { _id: state_id, locked: true,
+                    profile: cfg.profile, path: watch_path };
+          console.log('making new cfg for: '+watch_path);
+          var callback = this;
+          cfg.db.request('PUT', '/play/'+state_id, state, function(err, ret){
+            // FIXME needs error checking
+            state._rev = ret.rev;
+            callback(null, state);
+          });
+          return;
+        }
+        //else if (err.stack) {console.log(err.stack);return;}
+        //else if (err) cdie(err, 'could not get state');
+        else {
+          // We got state from couch... update!
+          state = ret;
+          state.locked = true;
+        }
+        cfg.db.save(state, this);
       },
       // Import file
       function import_file(err, res){
-        if(err)cdie(err,'bummer');
-        console.log('woot');
+        state = res;
+        if(err){console.log(err.stack);cdie(err,'bummer');};
+        console.log('locked '+state['path']);
+        console.log('state: '+JSON.stringify(res));
       }
       // Update status
       // Clear lock
