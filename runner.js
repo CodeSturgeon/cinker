db49945166fe550f3cc1814c553dcde6b3634e14
Nick Fisher <github@nickdafish.com>
2010-12-13 10:54:05 -0500

    First blush at getting node in line with new cinker ddoc

--- runner.js
+++ runner.js
@@ -2,6 +2,7 @@
 // Command script
 var fs = require('fs');
 var http = require('http');
+var util = require('util');
 
 var Step = require('step');
 var CouchClient = require('couch-client');
@@ -14,12 +15,35 @@ cfg.ddoc_uri = '/'+cfg.db_name+'/_design/cinker/';
 // FIXME set profile start and end keys
 cfg.profiles_uri = cfg.ddoc_uri + '_view/profiles?group=true'
 cfg.db = CouchClient('http://localhost:5984/play');
+cfg.cnx = http.createClient(5984, 'localhost');
 
 var cdie = function(err, message){
   console.log(message+' '+JSON.stringify(err));
   return null;
 }
 
+
+
+var req = cfg.cnx.request(cfg.profiles_uri);
+req.on('response', function(resp){
+  //console.log(util.inspect(resp));
+  var ret = '';
+  resp.on('data',function(chunk){ret += chunk;});
+  resp.on('end',function(){
+    var view = JSON.parse(ret);
+    watch_defs = view['rows'][0]['value'];
+    console.log(util.inspect(watch_defs));
+    for (wi in watch_defs) {
+      var _id = watch_defs[wi][0];
+      var path = watch_defs[wi][1];
+      console.log('Setting up for: '+path);
+      fs.watchFile(path, watchers.cinkWatch(_id, path, cfg));
+    }
+  });
+})
+req.end();
+
+/*
 var req_uri = cfg.profiles_uri+'&startkey='+JSON.stringify([cfg.profile])
 req_uri +='&endkey='+JSON.stringify([cfg.profile,{}]);
 
@@ -32,3 +56,4 @@ cfg.db.request('GET', req_uri, function(err, body) {
     fs.watchFile(watch_path, watchers.fileCink(watch_path, cfg));
   }
 });
+*/
--- watchers.js
+++ watchers.js
@@ -1,11 +1,80 @@
 var crypto = require('crypto');
 var fs = require('fs');
+var util = require('util');
 var Step = require('step');
+var mmh = require('./ddoc/lib/murmurhash2');
 
 var cdie = function(err, message){
   if(err.stack) console.log(message+'\n'+err.stack)
   else console.log(message+' '+JSON.stringify(err));
   return 'done';
+}
+
+var cinkWatch= function(_id, path, cfg){
+  var memo = 0;
+  var hash = '';
+  // FIXME upd_url should be encoded too
+  var upd_url = '/play/_design/cinker/_update/cink_up/'+_id;
+  //upd_url += '?profile='+cfg.profile+'&path='+escape(path);
+  upd_url += '?profile=test1&path='+escape(path);
+  console.log(upd_url);
+  return function(curr, prev){
+    // check that the mtime is updated, not just the atime
+    if (curr.mtime.toString() == memo){
+      console.log('killing rerun');
+      //console.log(util.inspect(curr));
+      //console.log(util.inspect(prev));
+      return;
+    }
+    memo = curr.mtime.toString();
+
+    console.log('go! '+path);
+    var ret = '';
+
+    Step(
+      // get new content
+      function(){
+        console.log('step0');
+        fs.readFile(path, 'utf8', this);
+      },
+      // send up content
+      function(err, content){
+        console.log('step1');
+        //console.log(content);
+
+        var new_hash = mmh.doHash(_id, content);
+        if (new_hash === hash) return console.log('nothing to do');
+        hash = new_hash;
+
+        var req = cfg.cnx.request('PUT',upd_url);
+        
+        // the response event has no err, so we have to inject one for Step
+        var callback = this;
+        req.on('response', function(resp){ callback(undefined, resp); });
+        req.end(content);
+      },
+      // process return
+      function(err, resp){
+        console.log('step2');
+        // Check for errors resp.statusCode
+        if (err) {
+          console.log('poo');
+          console.log(util.inspect(err));
+          console.log(err.message);
+        }
+        console.log(resp.statusCode);
+        console.log(util.inspect(resp));
+        resp.on('data', function(chunk){ret += chunk;});
+        resp.on('end', this)
+      },
+      // deal with output
+      function(err){
+        console.log('step3');
+        if (err) console.log(err.message);
+        console.log(ret);
+      }
+    );
+  }
 }
 
 // cfg: dict of running cfg
@@ -101,4 +170,5 @@ var fileCink = function(watch_path, cfg){
   }
 }
 
+exports.cinkWatch = cinkWatch;
 exports.fileCink = fileCink;
